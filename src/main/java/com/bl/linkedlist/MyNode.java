077164f309dddc0bf4832af42e8d0d1be7da9fec
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-23 17:03:55 +0530

    adding 30 and 56 to 70

--- src/main/java/com/bl/linkedlist/MyNode.java
+++ src/main/java/com/bl/linkedlist/MyNode.java
@@ -1,28 +1,43 @@
 package com.bl.linkedlist;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
 	
 	private K key;
-	private MyNode next;
+	private INode<K> next;
 	
 	public MyNode(K key) {
+		
 		this.key = key;
 		this.next = null;
 	}
-
-	public MyNode getNext() {
+	
+	@Override
+	public K getKey() {
+		return key;
+	}
+	
+	@Override
+	public void setKey(K key) {
+		this.key = key;
+	}
+	
+	public INode<K> getNext() {
 		return next;
 	}
-
-	public void setNext(MyNode next) {
+	
+	@Override
+	public void setNext(INode next) {
 		this.next = next;
 	}
 	
-	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode<Integer>(56);
-		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
-		firstNode.setNext(secondNode);
-		secondNode.setNext(thirdNode);
+	@Override
+	public String toString() {
+		StringBuilder myNodeString = new StringBuilder();
+		myNodeString.append("{"+"Key=").append(key).append('}');
+		
+		if(next!=null) {
+			myNodeString.append("->").append(next);
+		}
+		return myNodeString.toString();
 	}
 }
